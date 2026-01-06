afded9f83fba8b96866b18cf0d7daf62bcc2ca58
Sander van der Heijden <sander.van.der.heijden@alliander.com>
2026-01-06 13:02:19 +0100

    FDP-3267 ~ Fixes registration data serialization
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>

--- src/main/kotlin/org/opensmartgridplatform/dto/valueobjects/DeviceRegistrationDataDto.kt
+++ src/main/kotlin/org/opensmartgridplatform/dto/valueobjects/DeviceRegistrationDataDto.kt
@@ -8,7 +8,7 @@ import java.io.Serializable
 data class DeviceRegistrationDataDto(
     val ipAddress: String?,
     val deviceType: String?,
-    val hasSchedule: Boolean?,
+    val hasSchedule: Boolean,
 ) : Serializable {
     companion object {
         private const val serialVersionUID = -5264884250167476931L

4ef4845e440c7f46ef8ae6f9f2a12ed938c35520
Sander van der Heijden <sander.van.der.heijden@alliander.com>
2026-01-14 18:25:04 +0100

    FDP-3267 ~ Fixes spotless issue
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>

--- src/main/kotlin/org/lfenergy/gxf/publiclighting/message/transformer/common/ApplicationConstants.kt
+++ src/main/kotlin/org/lfenergy/gxf/publiclighting/message/transformer/common/ApplicationConstants.kt
@@ -5,6 +5,7 @@ package org.lfenergy.gxf.publiclighting.message.transformer.common
 
 object ApplicationConstants {
     const val JMS_PROPERTY_DEVICE_IDENTIFICATION = "DeviceIdentification"
+
     // Don't change this to "OrganizationIdentification", should be equal to value in platform
     const val JMS_PROPERTY_ORGANIZATION_IDENTIFICATION = "OrganisationIdentification"
     const val JMS_PROPERTY_NETWORK_ADDRESS = "NetworkAddress"

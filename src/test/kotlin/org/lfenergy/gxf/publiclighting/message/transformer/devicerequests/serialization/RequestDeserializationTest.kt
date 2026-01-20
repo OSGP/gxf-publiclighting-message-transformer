8d49a7fc4089507d88848a3954ad63f203032564
Jesse van der ent <jesse.van.der.ent@alliander.com>
2026-01-20 09:26:39 +0100

    feat: added configurationDTO test
    Signed-off-by: Jesse van der Ent <jesse.v.d.ent@hotmail.com>

--- src/test/kotlin/org/lfenergy/gxf/publiclighting/message/transformer/devicerequests/serialization/RequestDeserializationTest.kt
+++ src/test/kotlin/org/lfenergy/gxf/publiclighting/message/transformer/devicerequests/serialization/RequestDeserializationTest.kt
@@ -5,14 +5,20 @@ package org.lfenergy.gxf.publiclighting.message.transformer.devicerequests.seria
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
 import org.opensmartgridplatform.dto.valueobjects.ActionTimeTypeDto
+import org.opensmartgridplatform.dto.valueobjects.ConfigurationDto
 import org.opensmartgridplatform.dto.valueobjects.EventNotificationDto
 import org.opensmartgridplatform.dto.valueobjects.EventTypeDto
+import org.opensmartgridplatform.dto.valueobjects.LightTypeDto
+import org.opensmartgridplatform.dto.valueobjects.LinkTypeDto
 import org.opensmartgridplatform.dto.valueobjects.ScheduleDto
 import org.opensmartgridplatform.dto.valueobjects.TriggerTypeDto
 import org.opensmartgridplatform.dto.valueobjects.WeekDayTypeDto
 import java.io.ObjectInputStream
+import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.time.Instant
 
 class RequestDeserializationTest {
     /**
@@ -32,32 +38,81 @@ class RequestDeserializationTest {
                 .getResourceAsStream("/set-schedule-request.ser")
                 .use { ObjectInputStream(it).readObject() } as ScheduleDto
 
-        val scheduleOn = msg.scheduleList!![0]
-        assertThat(scheduleOn!!.weekDay).isEqualTo(WeekDayTypeDto.ALL)
+        assertNotNull(msg.scheduleList)
+        val scheduleOn = msg.scheduleList[0]
+        assertNotNull(scheduleOn)
+        assertThat(scheduleOn.weekDay).isEqualTo(WeekDayTypeDto.ALL)
         assertThat(scheduleOn.triggerType).isEqualTo(TriggerTypeDto.ASTRONOMICAL)
         assertThat(scheduleOn.actionTime).isEqualTo(ActionTimeTypeDto.SUNSET)
-        assertThat(scheduleOn.lightValue!![0]!!.index).isEqualTo(2)
-        assertThat(scheduleOn.lightValue[0]!!.on).isEqualTo(true)
+        assertNotNull(scheduleOn.lightValue)
+        assertThat(scheduleOn.lightValue[0]?.index).isEqualTo(2)
+        assertThat(scheduleOn.lightValue[0]?.on).isEqualTo(true)
 
         val scheduleOff = msg.scheduleList[1]
-        assertThat(scheduleOff!!.weekDay).isEqualTo(WeekDayTypeDto.ALL)
+        assertNotNull(scheduleOff)
+        assertThat(scheduleOff.weekDay).isEqualTo(WeekDayTypeDto.ALL)
         assertThat(scheduleOff.triggerType).isEqualTo(TriggerTypeDto.ASTRONOMICAL)
         assertThat(scheduleOff.actionTime).isEqualTo(ActionTimeTypeDto.SUNRISE)
-        assertThat(scheduleOff.lightValue!![0]!!.index).isEqualTo(2)
-        assertThat(scheduleOff.lightValue[0]!!.on).isEqualTo(false)
+        assertNotNull(scheduleOff.lightValue)
+        assertThat(scheduleOff.lightValue[0]?.index).isEqualTo(2)
+        assertThat(scheduleOff.lightValue[0]?.on).isEqualTo(false)
     }
 
     @Test
-    fun `should deserialize event notification request serialized by java`() {
+    fun `should deserialize configuration request serialized by java`() {
         val msg =
             javaClass
-                .getResourceAsStream("/event-notification-request.ser")
-                .use { ObjectInputStream(it).readObject() } as EventNotificationDto
+                .getResourceAsStream("/configuration-request.ser")
+                .use { ObjectInputStream(it).readObject() } as ConfigurationDto
 
-        assertThat(msg.deviceUid).isEqualTo("testUid")
-        assertThat(msg.eventType).isEqualTo(EventTypeDto.LIGHT_SENSOR_REPORTS_LIGHT)
-        assertThat(msg.description).isEqualTo("Sensor reports light")
-        assertThat(msg.index).isEqualTo(0)
-        assertThat(msg.dateTime).isEqualTo(ZonedDateTime.parse("2026-01-01T12:00:00+00:00"))
+
+        assertThat(msg.lightType).isEqualTo(LightTypeDto.RELAY)
+        assertThat(msg.daliConfiguration).isNotNull
+        assertThat(msg.relayConfiguration).isNotNull
+        assertThat(msg.preferredLinkType).isEqualTo(LinkTypeDto.CDMA)
+
+        assertThat(msg.timeSyncFrequency).isEqualTo(133)
+        assertThat(msg.dhcpEnabled).isTrue
+        assertThat(msg.tlsEnabled).isTrue
+        assertThat(msg.tlsPortNumber).isEqualTo(134)
+        assertThat(msg.commonNameString).isEqualTo("commonNameString1")
+        assertThat(msg.communicationTimeout).isEqualTo(135)
+        assertThat(msg.communicationNumberOfRetries).isEqualTo(136)
+        assertThat(msg.communicationPauseTimeBetweenConnectionTrials).isEqualTo(137)
+        assertThat(msg.osgpIpAddress).isEqualTo("osgpIpAddress1")
+        assertThat(msg.osgpPortNumber).isEqualTo(138)
+        assertThat(msg.ntpHost).isEqualTo("ntpHost1")
+        assertThat(msg.ntpEnabled).isTrue
+        assertThat(msg.ntpSyncInterval).isEqualTo(139)
+        assertThat(msg.testButtonEnabled).isTrue
+        assertThat(msg.automaticSummerTimingEnabled).isTrue
+        assertThat(msg.astroGateSunRiseOffset).isEqualTo(140)
+        assertThat(msg.astroGateSunSetOffset).isEqualTo(141)
+        assertThat(msg.relayRefreshing).isTrue
+
+        assertNotNull(msg.deviceFixedIp)
+        assertThat(msg.deviceFixedIp.ipAddress).isEqualTo("ipAddress1")
+        assertThat(msg.deviceFixedIp.netMask).isEqualTo("netMask1")
+        assertThat(msg.deviceFixedIp.gateWay).isEqualTo("gateWay1")
+
+        assertThat(msg.switchingDelays).containsExactly(142, 143)
+
+        assertThat(msg.relayLinking).hasSize(2)
+        val relayLinks = msg.relayLinking
+        assertNotNull(relayLinks)
+        assertThat(relayLinks[0]?.masterRelayIndex).isEqualTo(144)
+        assertThat(relayLinks[0]?.masterRelayOn).isTrue
+        assertThat(relayLinks[1]?.masterRelayIndex).isEqualTo(145)
+        assertThat(relayLinks[1]?.masterRelayOn).isFalse
+
+        val expectedSummer = ZonedDateTime.ofInstant(
+            Instant.ofEpochMilli(146L * 24 * 60 * 60 * 1000), ZoneId.systemDefault()
+        )
+        val expectedWinter = ZonedDateTime.ofInstant(
+            Instant.ofEpochMilli(147L * 24 * 60 * 60 * 1000), ZoneId.systemDefault()
+        )
+
+        assertThat(msg.summerTimeDetails).isEqualTo(expectedSummer)
+        assertThat(msg.winterTimeDetails).isEqualTo(expectedWinter)
     }
 }

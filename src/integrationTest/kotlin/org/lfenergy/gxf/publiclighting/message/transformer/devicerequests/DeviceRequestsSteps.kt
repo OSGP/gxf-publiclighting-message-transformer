142e3a8d26e6618690a123055900bb81e4f4abed
Sander van der Heijden <sander.van.der.heijden@alliander.com>
2026-01-21 14:35:26 +0100

    FDP-3267 ~ Fixes integration tests
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>

--- src/integrationTest/kotlin/org/lfenergy/gxf/publiclighting/message/transformer/devicerequests/DeviceRequestsSteps.kt
+++ src/integrationTest/kotlin/org/lfenergy/gxf/publiclighting/message/transformer/devicerequests/DeviceRequestsSteps.kt
@@ -18,12 +18,16 @@ import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.Transi
 import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.TriggerType
 import org.lfenergy.gxf.publiclighting.contracts.internal.device_requests.Weekday
 import org.lfenergy.gxf.publiclighting.message.transformer.common.ApplicationConstants.JMS_PROPERTY_DEVICE_IDENTIFICATION
+import org.lfenergy.gxf.publiclighting.message.transformer.common.ApplicationConstants.JMS_PROPERTY_DOMAIN
+import org.lfenergy.gxf.publiclighting.message.transformer.common.ApplicationConstants.JMS_PROPERTY_DOMAIN_VERSION
 import org.lfenergy.gxf.publiclighting.message.transformer.common.ApplicationConstants.JMS_PROPERTY_NETWORK_ADDRESS
 import org.lfenergy.gxf.publiclighting.message.transformer.common.ApplicationConstants.JMS_PROPERTY_ORGANIZATION_IDENTIFICATION
 import org.lfenergy.gxf.publiclighting.message.transformer.common.ObjectMessageType
 import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.CORRELATION_UID
 import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.DEFAULT_PRIORITY
 import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.DEVICE_IDENTIFICATION
+import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.DOMAIN
+import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.DOMAIN_VERSION
 import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.NETWORK_ADDRESS
 import org.lfenergy.gxf.publiclighting.message.transformer.common.TestConstants.ORGANIZATION_IDENTIFICATION
 import org.springframework.beans.factory.annotation.Qualifier
@@ -135,8 +139,10 @@ class DeviceRequestsSteps(
                 message.jmsCorrelationID = correlationUid
                 message.jmsPriority = DEFAULT_PRIORITY
                 message.setStringProperty(JMS_PROPERTY_DEVICE_IDENTIFICATION, deviceIdentification)
-                message.setStringProperty(JMS_PROPERTY_ORGANIZATION_IDENTIFICATION, organisationIdentification)
+                message.setStringProperty(JMS_PROPERTY_DOMAIN, DOMAIN)
+                message.setStringProperty(JMS_PROPERTY_DOMAIN_VERSION, DOMAIN_VERSION)
                 message.setStringProperty(JMS_PROPERTY_NETWORK_ADDRESS, ipAddress)
+                message.setStringProperty(JMS_PROPERTY_ORGANIZATION_IDENTIFICATION, organisationIdentification)
                 if (request != null) {
                     message.`object` = request
                 }
